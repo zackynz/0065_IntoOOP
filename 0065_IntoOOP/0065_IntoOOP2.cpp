a1527d681e9b0d8415ebe059255bd0c3dfd477f5
zackynz <nurasfanz@gmail.com>
2024-04-22 16:35:48 +0800

    memasukkan perintah untuk tipe data yang memili angka desimal

--- 0065_IntoOOP/0065_IntoOOP2.cpp
+++ 0065_IntoOOP/0065_IntoOOP2.cpp
@@ -2,12 +2,22 @@
 using namespace std;
 
 class bangunDatar {
+
+	
 	private:
 		float panjang, lebar;
 	public:
 		float luas;
 
 		void input() {
+
+			cout << "Masukan Panjangnya = ";
+			cin >> panjang;
+			cout << "Masukan Lebarnya = ";
+			cin >> lebar;
+		}
+
+		float hitungluas() {
 
 		}
 };
